918fb7e92d134b756e9b0a3a9fe7a8ce33592ddf
Martin Terribile <juanmartterribile@gmail.com>
2023-10-05 19:32:28 -0300

    WIP(about)

--- Portfolio/src/Views/About/About.jsx
+++ Portfolio/src/Views/About/About.jsx
@@ -1,8 +1,19 @@
+import styles from './About.module.css'
 
-const About = () =>{
-    return(
-        <div id="about" >
+const About = () => {
+    return (
+        <div className={styles.about} id="about" >
+            <div className={styles.about_text}>
+                <div className={styles.about_text_title}>
+                    <h2>So, who am I?</h2>
+                </div>
+                <div className={styles.about_text_description}>
+                    <p>I am a web developer based in Buenos Aires, Argentina, whose experience as a former officer in the Navy has enriched my approach to technology. The soft skills I acquired, such as leadership, adaptability, and decision-making under pressure, intertwine with my ability to create functional and aesthetically appealing web solutions. My commitment is to leverage this unique combination to deliver exceptional results in every project I undertake.</p>
+                </div>
+            </div>
+            <div className={styles.about_images}>
 
+            </div>
         </div>
     )
 }
