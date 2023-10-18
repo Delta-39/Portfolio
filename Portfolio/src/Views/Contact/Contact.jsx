1f58e25483815441ea8afb2e47496613caad960e
Martin Terribile <juanmartterribile@gmail.com>
2023-10-18 18:10:32 -0300

    WIP(contact, pagetransition to project detail)

--- Portfolio/src/Views/Contact/Contact.jsx
+++ Portfolio/src/Views/Contact/Contact.jsx
@@ -1,8 +1,12 @@
-import React from "react";
 import styles from "./Contact.module.css";
 import { FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Contact = () => {
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+  }
+
   return (
     <div className={styles.container} id="contact">
       <h2 className={styles.contactTitle}>Contact</h2>
@@ -10,30 +14,30 @@ const Contact = () => {
         <div className={styles.contactForm}>
           <form action="">
             <div className={styles.inputField}>
-              <input type="text" name="name" id="name" />
-              <label htmlFor="name">Name:</label>
+              <input type="text" name="name" id="name" required />
+              <label htmlFor="name">Name</label>
             </div>
             <div className={styles.inputField}>
-              <input type="email" name="email" id="email" />
-              <label htmlFor="email">Email:</label>
+              <input type="text" name="email" id="email" required/>
+              <label htmlFor="email">Email</label>
             </div>
-            <div>
-              <label htmlFor="message">Tell what can I help you with:</label>
+            <div className={styles.textareaContainer}>
+              <label htmlFor="message">Tell more about the Project</label>
               <textarea rows="5"></textarea>
             </div>
-            <button>Send</button>
+            <button onClick={handleSubmit} >Send</button>
           </form>
         </div>
         <div className={styles.container_socialNetwork}>
-          <h3>My social network</h3>
+          <h3>My social networks</h3>
           <div className={styles.socialNetwork}>
-            <a href="">
+            <a href="https://github.com/Delta-39">
               <FaGithub />
             </a>
-            <a href="">
+            <a href="https://www.linkedin.com/in/juan-martin-terribile-giles-90a4b1229/">
               <FaLinkedin />
             </a>
-            <a href="">
+            <a href="https://www.instagram.com/jmterribile/">
               <FaInstagram />
             </a>
           </div>
