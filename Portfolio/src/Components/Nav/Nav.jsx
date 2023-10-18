e2857e50515d1621334bc3063147a5981eee8dbe
Delta-39 <martinterribile@gmail.com>
2023-10-18 16:46:21 -0300

    cambio de pc

--- Portfolio/src/Components/Nav/Nav.jsx
+++ Portfolio/src/Components/Nav/Nav.jsx
@@ -3,7 +3,6 @@ import styles from './Nav.module.css'
 
 const Nav = () => {
 
-
     return (
         <header className={styles.header} >
             <div className={styles.title} >
--- Portfolio/src/Views/Contact/Contact.jsx
+++ Portfolio/src/Views/Contact/Contact.jsx
@@ -1,27 +1,46 @@
-import styles from "./Contact.module.css"
+import React from "react";
+import styles from "./Contact.module.css";
+import { FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
 
-const Contact = () =>{
-    return(
-        <div className={styles.container} id="contact" >
-            <h2>Contact Me!</h2>
-            <div>
-                <form action="">
-                    <div>
-                        <label htmlFor="name">Name:</label>
-                        <input type="text" name="name" />
-                    </div>
-                    <div>
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" name="email" />
-                    </div>
-                    
-                </form>
+const Contact = () => {
+  return (
+    <div className={styles.container} id="contact">
+      <h2 className={styles.contactTitle}>Contact</h2>
+      <div className={styles.contactContainer}>
+        <div className={styles.contactForm}>
+          <form action="">
+            <div className={styles.inputField}>
+              <input type="text" name="name" id="name" />
+              <label htmlFor="name">Name:</label>
+            </div>
+            <div className={styles.inputField}>
+              <input type="email" name="email" id="email" />
+              <label htmlFor="email">Email:</label>
             </div>
             <div>
-
+              <label htmlFor="message">Tell what can I help you with:</label>
+              <textarea rows="5"></textarea>
             </div>
+            <button>Send</button>
+          </form>
         </div>
-    )
-}
+        <div className={styles.container_socialNetwork}>
+          <h3>My social network</h3>
+          <div className={styles.socialNetwork}>
+            <a href="">
+              <FaGithub />
+            </a>
+            <a href="">
+              <FaLinkedin />
+            </a>
+            <a href="">
+              <FaInstagram />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
