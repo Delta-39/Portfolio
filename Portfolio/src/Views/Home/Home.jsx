fc30ee4f3795dc44d26460b36d6334041cd85c9b
Delta-39 <martinterribile@gmail.com>
2023-10-23 10:49:36 -0300

    FEAT(resume button functional)

--- Portfolio/src/Views/Home/Home.jsx
+++ Portfolio/src/Views/Home/Home.jsx
@@ -1,8 +1,42 @@
 import heroImage from "../../assets/Daco_49264431.png"
 import styles from "./Home.module.css"
-
+import axios from 'axios'
+import { API_URL } from "../../utils/routes"
+import { toast } from 'react-toastify'
 
 const Home = () => {
+    const handleClick = async () =>{
+        try {
+            const response = await axios.get(`${API_URL}/pdf`, {
+                responseType: 'blob', // Especifica que esperas una respuesta binaria (el archivo PDF)
+              });
+          
+              // Extrae el blob (archivo PDF) de la respuesta
+              const pdfBlob = response.data;
+          
+              // Crea un objeto URL a partir del blob para descargar el archivo
+              const url = window.URL.createObjectURL(pdfBlob);
+          
+              // Crea un enlace en el DOM para descargar el archivo
+              const a = document.createElement('a');
+              a.style.display = 'none';
+              a.href = url;
+              a.download = 'Martin Terribile.pdf'; // Nombre del archivo para la descarga
+              document.body.appendChild(a);
+          
+              // Simula un clic en el enlace para iniciar la descarga
+              a.click();
+          
+              // Libera el objeto URL después de la descarga
+              window.URL.revokeObjectURL(url);
+            toast.success('Congrats! Yhou download the best resume in the world!')
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
     return (
         <div>
             <div className={styles.container}>
@@ -13,7 +47,7 @@ const Home = () => {
                         <h3>I create websites carefully, focusing on both functionality and aesthetics.</h3>
                     </div>
                     <div data-aos="fade-up" data-aos-duration={1000} data-aos-delay={1300} data-aos-offset={-100} className={styles.container_left_buttons}>
-                        <button >Resume</button>
+                        <button onClick={handleClick} >Resume</button>
                         <button ><a href="https://www.linkedin.com/in/juan-martin-terribile-giles-90a4b1229/">Connect with Me</a></button>
                     </div>
                 </div>
