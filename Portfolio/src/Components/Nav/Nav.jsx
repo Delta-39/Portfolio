ef0950ae60ad31c16f9de57b948499d218f58c9b
Delta-39 <martinterribile@gmail.com>
2023-10-23 12:01:01 -0300

    WIP(responsive)

--- Portfolio/src/Components/Nav/Nav.jsx
+++ Portfolio/src/Components/Nav/Nav.jsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-scroll';
 import styles from './Nav.module.css'
+import { useState} from 'react'
+
 
 const Nav = () => {
+
+    const [showMenu, setShowMenu] = useState(false)
 
     return (
         <header id="home" className={styles.header} >
@@ -9,7 +13,12 @@ const Nav = () => {
                 <h1 data-aos="fade-left" data-aos-duration={1000} data-aos-delay={500} data-aos-easing="ease-in-out" >Martin Terribile {'</>'}</h1>
             </div>
             <div className={styles.navContainer} >
-                <nav className={styles.nav} >
+                <div>
+                    <nav className={styles.mobileNav} >
+
+                    </nav>
+                </div>
+                <nav className={styles.desktopNav} >
                     <ul className={styles.realNav} >
                         <li data-aos="fade-down" data-aos-duration={1000} data-aos-delay={300} ><Link className={styles.navLink} to="about"smooth={true} duration={1000}>About</Link></li>
                         <li data-aos="fade-down" data-aos-duration={1000} data-aos-delay={600} ><Link className={styles.navLink} to="skills" smooth={true} duration={1000}>Skills</Link></li>
