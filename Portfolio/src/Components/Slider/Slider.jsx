68c59a4ba32040f4f6078171d1cfe8fbfe26a274
Delta-39 <martinterribile@gmail.com>
2023-10-24 09:23:31 -0300

    finished

--- Portfolio/src/Components/Slider/Slider.jsx
+++ Portfolio/src/Components/Slider/Slider.jsx
@@ -4,8 +4,34 @@ import "slick-carousel/slick/slick-theme.css";
 import styles from './Slider.module.css'
 import projectData from "../../Views/Projects/projectsData";
 import { Link } from "react-router-dom";
+import './Slider.css'
+
+// Define los componentes prevArrow y nextArrow
+function CustomPrevArrow(props) {
+  const { className,style, onClick } = props;
+
+  return (
+    <div
+      className={className}
+      style={{...style}}
+      onClick={onClick}
+    />
+  );
+}
+
+
+function CustomNextArrow(props) {
+  const { className,onClick } = props;
+  return (
+    <div
+      className={className}
+      onClick={onClick}
+    />
+  );
+}
 
 const ProjectSlider = () => {
+
   const settings = {
     dots: true,
     infinite: true,
@@ -13,12 +39,14 @@ const ProjectSlider = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     adaptativeHeight:true,
+    prevArrow: <CustomPrevArrow className="prev-arrow"/>,
+    nextArrow: <CustomNextArrow />
   };
 
   return (
     <div data-aos="zoom-in-up" data-aos-duration={2000} data-aos-delay={400} className={styles.carouselContainer} >
       <Slider {...settings}>
-        {projectData.map((project,index)=>(
+        {projectData.map((project, index) => (
           <div key={index} className={styles.imageContainer} >
             <Link to={`/${project.id}`} >
               <img src={project.imageDesktop} alt={project.name} />
@@ -35,3 +63,4 @@ const ProjectSlider = () => {
 };
 
 export default ProjectSlider;
+
